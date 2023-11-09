16d59d74407a2758bb85f195888df027f1985de6
EsteEsChoque <86280354+EsteEsChoque@users.noreply.github.com>
2023-11-09 14:28:31 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+// cambia aca tus redes
+
+
+const redditLink = "https://www.reddit.com/";
+const instagramLink = "https://www.instagram.com";
+const twitterLink = "https://twitter.com/home";
+const faceBook = "https://www.facebook.com/";
+const snapChat = "https://www.snapchat.com/es";
+const onlyFans = "https://onlyfans.com/";
+
+
